9350fdb667c072da1f9e284597ac73a81066d4f3
zombie110year <zombie110year@outlook.com>
2019-05-09 01:40:56 +0800

    更换 build-pdf 工具

---
+++ tools/build-pdf.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/bash
+
+echo 需要安装 texlive 以及 Sarasa, Source Han 字体
+echo 在项目根目录以 ./tools/build-pdf.sh 的方式运行
+version='0.1.0rc'
+file_basename="c-and-cpp.v${version}"
+echo ${file_basename}
+
+cd build/latex
+xelatex "${file_basename}.tex"
+xelatex "${file_basename}.tex"
+
+cp "${file_basename}.pdf" ..
+# 放到 build 目录中
